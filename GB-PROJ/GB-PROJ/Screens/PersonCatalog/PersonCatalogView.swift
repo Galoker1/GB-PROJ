61fd3a2dfdd9d27b89a614b6ecd02225fde6bad2
Ga1oker <54763540+Galoker1@users.noreply.github.com>
2023-10-19 00:38:58 +0300

    Add favorites and serials

--- GB-PROJ/GB-PROJ/Screens/PersonCatalog/PersonCatalogView.swift
+++ GB-PROJ/GB-PROJ/Screens/PersonCatalog/PersonCatalogView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct PersonCatalogView: View {
-    @StateObject private var filmViewModel = FilmViewModel()
+    @StateObject private var filmViewModel = FilmViewModel(typeOfTitle: "movie")
     @StateObject private var tvShowViewModel = TVShowViewModel()
     let films: [MovieCD] = CoreDataManager.shared.getFilms()
     var body: some View {
@@ -19,33 +19,30 @@ struct PersonCatalogView: View {
             .padding(.horizontal, 16)
             Divider().background(Color.Primary.num1)
             List {
-                Section(header:
-                            CustomDisplayTextLabel(textLabel: "Фильмы",
-                                                   color: Color.Primary.num1,
-                                                   style: .T1DisplaySemibold)) {
-                    ForEach(filmViewModel.films, id: \.self) { movie in
-                        CustomTableViewCell(mediaItem: movie)
-
+                ForEach(films, id: \.self) { movie in
+                    LazyVStack(alignment: .leading){
+                        VStack(alignment: .leading) {
+                            HStack{
+                                Image(uiImage: UIImage(data: movie.poster ?? Data()) ?? UIImage())
+                                    .resizable()
+                                    .padding(.vertical, 2)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 40)
+                                Text(movie.name ?? " ")
+                                    .font(.system(size: 16))
+                                    .lineLimit(1)
+                                    .foregroundColor(Color.white)
+                            }
+                        }
                     }
-                    .onDelete { indexSet in
-                        deleteItems($filmViewModel.films, at: indexSet)
+                    .listRowBackground(Color.Neutral.num2)
+                    .swipeActions {
+                        Button("Удалить", role: .destructive) {
+                            CoreDataManager.shared.deleteById(id: movie.titleId)
+                        }
                     }
-                } .listRowBackground(Color.Neutral.num2)
-                
-                Section(header:
-                            CustomDisplayTextLabel(textLabel: "Сериалы",
-                                                   color: Color.Primary.num1,
-                                                   style: .T1DisplaySemibold)) {
-                    ForEach(tvShowViewModel.tvShow, id: \.self) { series in
-                        CustomTableViewCell(mediaItem: series)
-                        
-                    }  .onDelete { indexSet in
-                        deleteItems($tvShowViewModel.tvShow, at: indexSet)
-                    }
-                    
-                }.listRowBackground(Color.Neutral.num2)
+                }
             }
-            .listStyle(InsetListStyle())
             .background(Color.Neutral.num3)
             .scrollContentBackground(.hidden)
         }
@@ -53,41 +50,12 @@ struct PersonCatalogView: View {
         
     }
     
-    func deleteItems<T>(_ items: Binding<[T]>, at offsets: IndexSet) where T: Identifiable {
-        items.wrappedValue.remove(atOffsets: offsets)
-    }
+    
+//    struct PersonCatalogView_Previews: PreviewProvider {
+//        static var previews: some View {
+//            PersonCatalogView()
+//                .environmentObject(FilmViewModel())
+//                .environmentObject(TVShowViewModel())
+//        }
+//    }
 }
-
-//
-//    var body: some View {
-//        VStack {
-//            CustomHeaderView(textLabel: TabBarProperties.personCatalog.tabBarTitle)
-//                .padding(.horizontal, 16)
-//
-//            //TODO: - сдела заглушки, как будут отделены фильмы от сериалов, нужно будет запихнуть данные сюда
-//            Text("Количество:\(CoreDataManager.shared.getFilms().count)")
-//            ScrollView {
-//                ForEach(films, id: \.self) { movie in
-//                    LazyVStack(alignment: .leading){
-//                        VStack(alignment: .leading) {
-//                            ZStack{
-//
-//                                Text(movie.name ?? " ")
-//                                    .font(.system(size: 16))
-//                                    .lineLimit(1)
-//                            }
-//
-//                        }
-//                        .foregroundColor(Color.white)
-//                        .padding()
-//                        .cornerRadius(10)
-//                        .frame(height: 250)
-
-
-struct PersonCatalogView_Previews: PreviewProvider {
-    static var previews: some View {
-        PersonCatalogView()
-            .environmentObject(FilmViewModel())
-            .environmentObject(TVShowViewModel())
-        }
-    }
